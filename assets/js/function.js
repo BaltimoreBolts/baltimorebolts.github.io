16906a926c7151d489fec42d3ca6f64fb1a8e0c3
EthanJoyner <el2joyner@gmail.com>
2023-12-09 23:22:41 -0500

    2024 Countdown update Also failed attempt of putting the FIrst in show video up -E

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Jan 9, 2021 00:00:00").getTime();
+var countDownDate = new Date("Jan 6, 2024 00:00:00").getTime();
 
 var x = setInterval(function () {
 
--- index.md
+++ index.md
@@ -6,4 +6,4 @@ layout: default
 {% include banner.html %}
 {% include about.html %}
 {% include skills.html %}
-{% include gameVideo.html %}
+{% include countdown.html%}
